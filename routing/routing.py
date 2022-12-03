f46853ae6db0c73b0ecb87a02a5c506e16c62ce7
Joonas Coatanea <jcoatanea@protonmail.com>
2022-12-03 20:21:02 +0200

    Added a custom css stylesheet, added a template for the login page, added a route for the login page, other minor changes to the header and layout templates

--- routing/routing.py
+++ routing/routing.py
@@ -6,3 +6,8 @@ from app import app
 @app.route("/")
 def home_route():
     return render_template("page/home.html")
+
+
+@app.route("/login")
+def login_route():
+    return render_template("page/login.html")
