ae4263e4a42d2145b31138049f8fdb7f47a6a0a6
Joonas Coatanea <jcoatanea@protonmail.com>
2022-12-18 20:46:16 +0100

    Added a template for the create new message chain page, added a route for the create new message chain page, added a route to handle submitting the message chain creation form, fixed a bug with the Jinja macro to render an element if the user has insufficient permissions, other changes and improvements

--- routing/routing.py
+++ routing/routing.py
@@ -4,13 +4,14 @@ from flask import url_for, abort, redirect, render_template, request, session
 from sqlalchemy.exc import IntegrityError
 
 from app import app
+from dto.message_chain import MessageChain
 from dto.ui_error import UiError
 from dto.user import User
 from services.csrf_token_service import csrf_token_service
 from services.message_chain_service import message_chain_service
 from services.topic_service import topic_service
 from services.user_service import user_service
-from utils.session_utils import store_logged_in_user_into_session
+from utils.session_utils import fetch_id_of_logged_in_user_from_session, store_logged_in_user_into_session
 
 
 @app.before_request
@@ -68,6 +69,54 @@ def message_chain_list_route(topic_id):
         form_values={"message_chain_name_search_input": message_chain_name_search_filter},
         csrf_token=csrf_token_service.get_csrf_token()
     )
+
+
+@app.get("/topic/<int:topic_id>/message_chain/new")
+def create_new_message_chain_route(topic_id):
+    return render_template(
+        "page/create_new_message_chain.html",
+        topic_id=topic_id,
+        csrf_token=csrf_token_service.get_csrf_token()
+    )
+
+
+@app.post("/topic/<int:topic_id>/message_chain/new")
+def create_new_message_chain_submit_route(topic_id):
+    if not csrf_token_service.verify_request():
+        abort(403)
+
+    name = request.form["name"]
+
+    if not name:
+        return render_template(
+            "page/create_new_message_chain.html",
+            topic_id=topic_id,
+            error=UiError("This field is required", "name_input"),
+            form_values={
+                "name_input": name
+            },
+            csrf_token=csrf_token_service.get_csrf_token()
+        )
+
+    try:
+        message_chain_service.create_message_chain(
+            MessageChain(name, topic_id, fetch_id_of_logged_in_user_from_session())
+        )
+    except IntegrityError as exc:
+        if "name" in exc.orig.args[0] and "already exists" in exc.orig.args[0]:
+            return render_template(
+                "page/create_new_message_chain.html",
+                topic_id=topic_id,
+                error=UiError("A message chain with this name already exists", "name_input"),
+                form_values={
+                    "name_input": name
+                },
+                csrf_token=csrf_token_service.get_csrf_token()
+            )
+
+        raise exc
+
+    return redirect(url_for("message_chain_list_route", topic_id=topic_id))
 
 
 @app.get("/login")
